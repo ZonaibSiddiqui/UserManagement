9b41adbda634823ddbf0fc9e2fd29f167c576ffd
zonaib.siddiqui <zonaib.siddiqui@zintechonologies.com>
2013-04-04 09:09:33 +0500

    some ui changes

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -7,8 +7,8 @@
  */
 
 
-Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
-    xtype: 'Main',
+Ext.define('UserManagement.view.main.Main', {extend: 'Ext.container.Container',
+    xtype: 'main',
     renderTo: Ext.getBody(),
     id: 'MainID',
 //    main view layout vbox
@@ -31,20 +31,29 @@ Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
             },
             items: [
                 {
-                    xtype: 'label',
-                    text: 'User Name',
-                    margin: '10px 0 0 1040px',
-                    cls: 'whiteLabelTextCls'
+                    xtype : 'label',
+                    flex : 14.5
+                },{
+                    xtype : 'button',
+                    overCls  : 'logoutBtn',
+                    pressedCls : 'logoutBtn',
+                    text: 'Zonaib Siddiqui',
+                    cls: 'whiteLabelTextCls whiteLabelTextFontCls',
+                    action : 'userNameBtnAction'
 
                 },
                 {
                     xtype : 'button',
                     text  : 'Logout',
-                    margin : '0 0 0 5%',
+//                    margin : '0 0 0 2%',
+//                    flex : 1,
                     cls : 'logoutbtnCls',
                     overCls  : 'logoutBtn',
                     pressedCls : 'logoutBtn',
                     action : 'loginActionBtn'
+                },{
+                    xtype : 'label',
+                    flex : 2
                 }
             ]
         },
@@ -60,7 +69,7 @@ Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
             items: [
                 {
                     xtype: 'tbspacer',
-                    width: 134
+                    width: 148
                 },
                 {
                     xtype: 'image',
@@ -89,35 +98,27 @@ Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
                     items: [
                         {
                             xtype: 'label',
-                            width: 135,
+                            width: 155,
                             height: 50,
                             cls: 'mainSenchaLogoImgCls'
 
                         },
                         {
                             xtype: 'label',
-                            width: 40,
-                            height: 80,
+                            width: '100%',
+//                            height: 800,
+                            flex : 40,
                             cls: 'senchaMainWhiteClr'
                         }
                     ]
                 },
                 {
-//                    grid view added in the middle of vbo
-//                    xtype : 'container',
-//                    cls : 'borderRaduisConCls',
-//                    items : [
-//                        {
-                            xtype: 'UserList',
-                            id : 'mainUserListId',
-                            width : '20%',
-                            margin: '10% 10% 0 10%',
-                            border: 1,
-                            style: 'border-radius: 5px;',
-                            flex: 1
-//                        }
-//
-//                    ]
+                    xtype: 'userList',
+                    id : 'mainUserListId',
+                    width : '20%',
+                    border: 1,
+                    style: 'border-radius: 5px;',
+                    flex: 1
                 },
                 {
 //                    left container
@@ -129,14 +130,14 @@ Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
                     items: [
                         {
                             xtype: 'label',
-                            width: 135,
+                            width: 155,
                             height: 50,
                             cls: 'mainSenchaLogoImgCls'
 
                         },
                         {
                             xtype: 'label',
-                            width: 40,
+                            width: '100%',
                             height: 80,
                             cls: 'senchaMainWhiteClr'
                         }
@@ -147,7 +148,7 @@ Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
         {
             xtype: 'label',
             flex : 65,
-            height : 200
+            height : 2
 //            text : 'i m here'
 //            height :  30
 //            flex : 4
@@ -167,7 +168,7 @@ Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
 //            margin :
             border: 1,
             style: {
-                borderColor: '#B9B9B9',
+                borderColor: '#C8C8C8',
                 borderStyle: 'solid'
             },
             items: [
@@ -179,9 +180,9 @@ Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
                     flex: 2,
                     xtype: 'label',
 
-                    text: '@Zonaib inc, all rights are not reserved.',
+                    text: '@2013 Sencha Inc. All rights are reserved.',
                     dock: 'bottom',
-                    style: 'color :#A399AE',
+                    style: 'color :#646464',
                     margin: '20 0 0 0'
                 },
                 {
@@ -191,5 +192,6 @@ Ext.define('SenchaLogin.view.main.Main', {extend: 'Ext.container.Container',
             ]
         }
     ]
+
 });
 
